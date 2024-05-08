ca078ce7bed16be5f1b7960cd7e48dfd6965a26a
kojok <kokamoto@student.42tokyo.jp>
2024-05-08 23:04:52 +0900

    fuck

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: kokamoto <kokamoto@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 21:43:08 by kokamoto          #+#    #+#             */
-/*   Updated: 2024/04/27 16:10:25 by kokamoto         ###   ########.fr       */
+/*   Updated: 2024/05/08 22:56:58 by kokamoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
 	size_t	dest_len;
 	size_t	src_len;
 
+	if (destsize == 0)
+		return (ft_strlen(src));
 	dest_len = ft_strlen(dest);
 	src_len = ft_strlen(src);
 	i = 0;
@@ -33,3 +35,12 @@ size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
 	dest[dest_len + i] = '\0';
 	return (dest_len + src_len);
 }
+
+// int main(void)
+// {
+// 	char src1[] = "rhori";
+// 	printf("%zu\n", strlcat(NULL, src1, 0));
+// 	printf("%zu\n", ft_strlcat(NULL, src1, 0));
+// 	printf("%zu\n", strlcat(src1, NULL, 0));
+// 	printf("%zu\n", ft_strlcat(src1, NULL, 0));
+// }
