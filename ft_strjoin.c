3b1f7fd7fe85ff69ab837a4f2ecbe363b11753a3
岡本紘治 <>
2024-07-09 19:43:39 +0900

    なぜかstrjoinがコンパイルエラーになっていた

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kokamoto <kokamoto@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kokamoto <kokamoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 21:21:12 by kokamoto          #+#    #+#             */
-/*   Updated: 2024/05/08 21:24:07 by kokamoto         ###   ########.fr       */
+/*   Updated: 2024/07/09 19:42:24 by kokamoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		i;
-	int		j;
 	char	*array;
 
-	i = 0;
-	j = 0;
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	if (s1 == NULL)
@@ -33,11 +29,3 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ft_strlcpy(array + ft_strlen(s1), s2, ft_strlen(s2) + 1);
 	return (array);
 }
-
-// int main (void)
-// {
-// 	printf("%s\n", ft_strjoin(NULL, NULL));
-// 	printf("%s\n", ft_strjoin("a", NULL));
-// 	printf("%s\n", ft_strjoin(NULL, "a"));
-// 	printf("%s\n", ft_strjoin("a", "a"));
-// }
