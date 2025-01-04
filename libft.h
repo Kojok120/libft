dbaf426d67b37e1e5763f0b2924331c2031813f5
Koji Okamoto <kokamoto@c2r1s1.42tokyo.jp>
2025-01-04 13:26:35 +0900

    OPEN_MAXを1024で定義

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kokamoto <kojokamo120@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 22:37:05 by kokamoto          #+#    #+#             */
-/*   Updated: 2025/01/02 13:24:15 by kokamoto         ###   ########.fr       */
+/*   Updated: 2025/01/04 13:08:29 by kokamoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@
 # endif
 
 # ifndef OPEN_MAX
-#  define OPEN_MAX _SC_OPEN_MAX
+#  define OPEN_MAX 1024
 # endif
 
 int					ft_isalpha(int c);
